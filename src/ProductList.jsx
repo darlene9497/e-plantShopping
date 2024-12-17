d0db82f8fa1d70a09a0f312ca96fa4f3fc4ca556
darlene9497 <darlene.nandabi@student.moringaschool.com>
2024-12-17 11:01:20 +0300

    :lipstick: design cleanup

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -219,18 +219,19 @@ function ProductList() {
     display: 'flex',
     justifyContent: 'space-between',
     alignIems: 'center',
-    fontSize: '20px',
+    fontSize: '16px',
    }
    const styleObjUl={
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '1100px',
+    width: '800px',
    }
    const styleA={
     color: 'white',
-    fontSize: '30px',
+    fontSize: '15px',
     textDecoration: 'none',
+    fontWeight: 'bold',
    }
    const handleCartClick = (e) => {
     e.preventDefault();
@@ -263,7 +264,33 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div>
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 256 256"
+                            id="IconChangeColor"
+                            height="50"
+                            width="50"
+                        >
+                            <rect width="156" height="156" fill="none"></rect>
+                            <circle cx="80" cy="216" r="12" fill="none" stroke="#FFFFFF" strokeWidth="7"></circle>
+                            <circle cx="184" cy="216" r="12" fill="none" stroke="#FFFFFF" strokeWidth="7"></circle>
+                            <path
+                            d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
+                            fill="none"
+                            stroke="#FFFFFF"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="7"
+                            id="mainIconPathAttribute"
+                            ></path>
+                        </svg>
+                        </h1>
+                    </a>
+                </div>
+
             </div>
         </div>
         {!showCart? (
